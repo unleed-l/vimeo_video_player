3ecbfa390f814642f18fe8689f4e9567089fea19
Belmokhtar Idir <belmokhtar_idir.fs@univ-boumerdes.dz>
2024-10-10 00:46:28 +0100

    add flick_portrait_with_settings_controls and switch qualities

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,7 +26,7 @@ class MyHomePageState extends State<MyHomePage> {
   /// also support url like this: 'www.vimeo.com/70591644', 'vimeo.com/70591644'
   // final String _vimeoVideoUrl = 'https://vimeo.com/70591644';
   final String _vimeoVideoUrl = 'https://player.vimeo.com/video/670821472'; //! The video must have mp4 files with quality (SD, HD, 360, 720, ...).
-  final String _vimeoToken = 'b546a1231d94bf0b8e5a1c32eb3cd244'; //! Vimeo have policy settings for developer API, you need to generate your token to access to your vedio.
+  final String _vimeoToken = ''; //! Vimeo have policy settings for developer API, you need to generate your token to access to your vedio.
 
       // Create Dio options with the headers
   @override
@@ -38,7 +38,6 @@ class MyHomePageState extends State<MyHomePage> {
           url: _vimeoVideoUrl,
           autoPlay: true,
           token: _vimeoToken,
-          
         ),
       ),
     );
